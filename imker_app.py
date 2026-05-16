04e42caee2edaca12a9f272b8f9f4e61d47950a4
Bastian-Bongratz <die4bongratz@gmail.com>
2026-05-16 13:01:13 +0200

    Update imker_app.py

--- imker_app.py
+++ imker_app.py
@@ -6,15 +6,16 @@ from datetime import datetime
 
 # --- GOOGLE SHEETS VERBINDUNG ---
 def hole_google_tabelle():
-   # ERSETZE ES DURCH DIESEN BLOCK:
-scope = ["https://www.googleapis.com/auth/spreadsheets"]
-# Hier reparieren wir eventuelle Formatierungsfehler im Schlüssel automatisch:
-info = dict(st.secrets["gcp_service_account"])
-if "private_key" in info:
-    info["private_key"] = info["private_key"].replace("\\n", "\n")
-
-creds = Credentials.from_service_account_info(info, scopes=scope)
-client = gspread.authorize(creds)
+    # Holt sich die Zugangsdaten aus den Streamlit Secrets
+    scope = ["https://www.googleapis.com/auth/spreadsheets"]
+    
+    # Hier reparieren wir eventuelle Formatierungsfehler im Schlüssel automatisch:
+    info = dict(st.secrets["gcp_service_account"])
+    if "private_key" in info:
+        info["private_key"] = info["private_key"].replace("\\n", "\n")
+        
+    creds = Credentials.from_service_account_info(info, scopes=scope)
+    client = gspread.authorize(creds)
     
     # Öffnet deine Google-Tabelle (Name muss exakt stimmen!)
     return client.open("Imker_Daten")
@@ -74,10 +75,10 @@ elif kategorie == "📋 Bestandsbuch":
     v_liste = st.text_input("Welche Völker? (z.B. 1,2,3)")
     mittel = st.text_input("Arzneimittel & Charge")
     menge = st.text_input("Dosierung")
-    wartezeit = st.number_input("Wartezeit (Tage)", 0)
+    warzeit = st.number_input("Wartezeit (Tage)", 0)
     
     if st.button("Bestandsbuch-Eintrag"):
-        daten = {"Völker": v_liste, "Mittel": mittel, "Menge": menge, "Wartezeit": wartezeit}
+        daten = {"Völker": v_liste, "Mittel": mittel, "Menge": menge, "Wartezeit": warzeit}
         speichere_in_google("Bestandsbuch", daten)
 
 elif kategorie == "🍯 Honigernte":
